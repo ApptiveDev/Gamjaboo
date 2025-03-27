190b0da391b65d182fc1c001f19ae5786145c445
sul1074 <sul107485@gmail.com>
2025-03-27 12:21:26 +0900

    스프링 환경설정

---
+++ src/main/java/com/example/gamjaboo/GamjaBooApplication.java
@@ -0,0 +1,13 @@
+package com.example.gamjaboo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamjaBooApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GamjaBooApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/gamjaboo/GamjaBooApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gamjaboo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamjaBooApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
