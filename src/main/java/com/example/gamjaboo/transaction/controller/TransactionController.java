25908cacbf101c2c1c45ab57935b05e101fb3249
sul1074 <sul107485@gmail.com>
2025-05-19 15:23:47 +0900

    feat/transaction-delete
    거래 내역 제거 기능 추가

--- src/main/java/com/example/gamjaboo/transaction/controller/TransactionController.java
+++ src/main/java/com/example/gamjaboo/transaction/controller/TransactionController.java
@@ -5,6 +5,7 @@ import com.example.gamjaboo.transaction.dto.TransactionRequestDto;
 import com.example.gamjaboo.transaction.dto.TransactionResponseDto;
 import com.example.gamjaboo.transaction.service.TransactionService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -12,21 +13,23 @@ import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
 import java.util.List;
+import java.util.Map;
 
 
 @RestController
 @RequestMapping("/api/transaction")
 @RequiredArgsConstructor
+@Slf4j
 public class TransactionController {
 
     private final TransactionService transactionService;
 
     @PostMapping("/record")
-    public ResponseEntity<ApiResponse<Void>> recordTransaction(@RequestBody TransactionRequestDto dto) {
-        transactionService.record(dto);
+    public ResponseEntity<ApiResponse<Map<String, Integer>>> recordTransaction(@RequestBody TransactionRequestDto dto) {
+        Map<String, Integer> response = Map.of("transactionId", transactionService.record(dto));
 
         return ResponseEntity.status(HttpStatus.CREATED)
-                .body(new ApiResponse<>(201, "success", "일일 거래 등록 성공", null));
+                .body(new ApiResponse<Map<String, Integer>>(201, "success", "일일 거래 등록 성공", response));
     }
 
     @GetMapping
@@ -38,6 +41,13 @@ public class TransactionController {
         return ResponseEntity.ok(new ApiResponse<>(200, "success", "일일 거래 목록 조회 성공", response));
     }
 
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<ApiResponse<Void>> removeTransaction(@PathVariable("id") Long id) {
+        transactionService.removeTransaction(id);
+
+        return ResponseEntity.ok(new ApiResponse<>(200, "success", "거래 내역 삭제 성공", null));
+    }
 
 
     @ExceptionHandler(IllegalArgumentException.class)
