edd49880d1941af3e07fdf699400f100450446cf
sul1074 <sul107485@gmail.com>
2025-05-26 22:09:33 +0900

    feat/일일 예산 조회 기능 추가

--- src/main/java/com/example/gamjaboo/budget/controller/DailyBudgetController.java
+++ src/main/java/com/example/gamjaboo/budget/controller/DailyBudgetController.java
@@ -1,11 +1,15 @@
 package com.example.gamjaboo.budget.controller;
 
+import com.example.gamjaboo.budget.dto.BudgetResponseDto;
 import com.example.gamjaboo.common.ApiResponse;
 import com.example.gamjaboo.budget.dto.BudgetRequestDto;
 import com.example.gamjaboo.budget.service.DailyBudgetService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDate;
 
 @RestController
 @RequestMapping("/api")
@@ -19,6 +23,16 @@ public class DailyBudgetController {
 
         ApiResponse<Void> response = new ApiResponse<>(201, "success", "예산 등록 성공", null);
         return ResponseEntity.status(201).body(response);
+    }
+
+    @GetMapping("/budget/{date}")
+    public ResponseEntity<ApiResponse<BudgetResponseDto>> getBudget(
+            @RequestParam("kakaoId") Long kakaoId,
+            @PathVariable("date") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate date) {
+
+        BudgetResponseDto dto = dailyBudgetService.getDailyBudget(kakaoId, date);
+
+        return ResponseEntity.ok(new ApiResponse<>(200, "success", "일일 예산 조회 성공", dto));
     }
 
     @ExceptionHandler(IllegalArgumentException.class)
