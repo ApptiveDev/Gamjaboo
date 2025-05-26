932f694fe3561c63c46a64b432d8ba7dd69bdf05
sul1074 <sul107485@gmail.com>
2025-05-26 20:26:04 +0900

    feat/주간 거래 내역 조회 기능 추가

--- src/main/java/com/example/gamjaboo/stats/controller/StatsController.java
+++ src/main/java/com/example/gamjaboo/stats/controller/StatsController.java
@@ -2,8 +2,10 @@ package com.example.gamjaboo.stats.controller;
 
 import com.example.gamjaboo.common.ApiResponse;
 import com.example.gamjaboo.stats.dto.DailyStatsDto;
+import com.example.gamjaboo.stats.dto.PeriodStatsDto;
 import com.example.gamjaboo.stats.service.StatsService;
 import lombok.RequiredArgsConstructor;
+import org.apache.coyote.Response;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -17,6 +19,7 @@ public class StatsController {
 
     private final StatsService statsService;
 
+    // 일일 거래 통계 조회
     @GetMapping("/daily/{date}")
     public ResponseEntity<ApiResponse<DailyStatsDto>> getDailyStats(
             @RequestParam("kakaoId") Long kakaoId,
@@ -25,6 +28,18 @@ public class StatsController {
         DailyStatsDto dto = statsService.getDailyStats(kakaoId, date);
 
         return ResponseEntity.ok(new ApiResponse<>(200, "success", "일일 거래 통계 조회 성공", dto));
+    }
+
+
+    // 주간 거래 통계 조회
+    @GetMapping("weekly/{date}")
+    public ResponseEntity<ApiResponse<PeriodStatsDto>> getWeeklyStats(
+            @RequestParam("kakaoId") Long kakaoId,
+            @PathVariable("date") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate date) {
+
+        PeriodStatsDto dto = statsService.getWeeklyStats(kakaoId, date);
+
+        return ResponseEntity.ok(new ApiResponse<>(200, "success", "주간 거래 통계 조회 성공", dto));
     }
 
     @ExceptionHandler(IllegalArgumentException.class)
