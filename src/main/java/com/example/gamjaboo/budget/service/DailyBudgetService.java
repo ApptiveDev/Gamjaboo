8421edac8c2a878c73f4c524c932fe790034fb7a
sul1074 <sul107485@gmail.com>
2025-05-26 22:34:32 +0900

    feat/일일 예산 삭제 기능 추가

--- src/main/java/com/example/gamjaboo/budget/service/DailyBudgetService.java
+++ src/main/java/com/example/gamjaboo/budget/service/DailyBudgetService.java
@@ -15,6 +15,7 @@ import java.util.Optional;
 public class DailyBudgetService {
     private final DailyBudgetRepository budgetRepository;
 
+    // 일일 예산 등록
     public void register(BudgetRequestDto dto) {
         if (budgetRepository.findByKakaoIdAndDate(dto.getKakaoId(), dto.getDate()).isPresent()) {
             throw new IllegalArgumentException("이미 등록된 예산입니다.");
@@ -29,6 +30,7 @@ public class DailyBudgetService {
         budgetRepository.save(budget);
     }
 
+    // 일일 예산 조회
     public BudgetResponseDto getDailyBudget(Long kakaoId, LocalDate date) {
         DailyBudget budget = budgetRepository.findByKakaoIdAndDate(kakaoId, date)
                 .orElseThrow(() -> new IllegalArgumentException("등록된 일일 예산이 없습니다"));
@@ -42,4 +44,11 @@ public class DailyBudgetService {
         );
     }
 
+    // 일일 예산 삭제
+    public void deleteBudget(Long kakaoId, LocalDate date) {
+        DailyBudget budget = budgetRepository.findByKakaoIdAndDate(kakaoId, date)
+                .orElseThrow(() -> new IllegalArgumentException("해당 날짜의 예산이 존재하지 않습니다"));
+
+        budgetRepository.delete(budget);
+    }
 }
